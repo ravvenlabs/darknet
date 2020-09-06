6779975cc03f57d5a32042ce9afd4e70ba18b87f
Brian <bxl1703@rit.edu>
2020-09-06 14:52:03 -0400

    Simulate program finalized

--- build/darknet/x64/simulate.py
+++ build/darknet/x64/simulate.py
@@ -2,7 +2,7 @@
 import pdb
 
 frameBuffer = [None]*10
-DisplayBuffer = []
+DisplayBuffer = [None]*10
 
 MVBuffer = [None]*10
 
@@ -17,8 +17,9 @@ def displayFrame(frameContent):
 
 def calcMVs(curFrame):
     return "MV"+str(curFrame)+" to "+str(curFrame+1)
-
+lastYolo = 0
 for frameIndex in range(0,frames):
+    
     
     curFrame=frameIndex+1
     #Push current frame to frame stack for display
@@ -26,28 +27,39 @@ for frameIndex in range(0,frames):
     if(frameIndex>9):
         ##We have full buffers, dsiplay frames
         print("At index: ", frameIndex, "Display: ", DisplayBuffer[frameIndex%FRAME_SKIP])
+        #print(DisplayBuffer)
     
     
-    MVBuffer[frameIndex%FRAME_SKIP] = calcMVs(frameIndex)
     frameBuffer[frameIndex%FRAME_SKIP] = frameIndex
     
 
     #If we are not at a yolo result frame
     if(not curFrame%FRAME_SKIP==0):
         ##Calulate MVs
-        pass
+        MVBuffer[frameIndex%FRAME_SKIP] = calcMVs(frameIndex)
         
     if(curFrame%FRAME_SKIP==0):
         print("Yolo finished at frame index: "+str(frameIndex))
+        
+        
         print("Motion vector past ", FRAME_SKIP," frames")
         print(MVBuffer)
         print("Frames to show past ", FRAME_SKIP," frames")
         print(frameBuffer)
         #Apply motion vectors to Detections
         
+        for bufIdx in range(0, FRAME_SKIP):
+            print(bufIdx)
+            #apply mot vec[i]
+            DisplayBuffer[bufIdx]=str(lastYolo) + " dets and move with " + str(MVBuffer[bufIdx])
+        
+        DisplayBuffer[FRAME_SKIP-1] = "Yolo from frame " + str(curFrame - FRAME_SKIP)
+        
         MVBuffer = [None]*10
-        DisplayBuffer = frameBuffer.copy()
+        #DisplayBuffer #= frameBuffer.copy()
         #MVBuffer.clear()
+        lastYolo = curFrame
+        #start next yolo
     
     #There is a 10 frame delay
     
